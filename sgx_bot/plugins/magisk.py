78ffce3c2b74e802a44f92b28ff2f7ef19c36a22
KassemSYR <kassemkasemkk@gmail.com>
2020-11-11 19:14:36 +0200

    magisk: fix repo

--- sgx_bot/plugins/magisk.py
+++ sgx_bot/plugins/magisk.py
@@ -8,11 +8,12 @@ from pyrogram import Client, filters
 async def magisk(bot, update):
     url = 'https://raw.githubusercontent.com/topjohnwu/magisk_files/'
     message = "Latest Magisk Releases:\n"
-    for magisk_type, path  in {"Stable":"master/stable", "Beta":"master/beta", "Canary (debug)":"canary/debug"}.items():
+    for magisk_type, path  in {"Stable":"master/stable", "Beta":"master/beta", "Canary":"canary/canary"}.items():
+        canary = "https://github.com/topjohnwu/magisk_files/raw/canary/" if magisk_type == "Canary" else ""
         data = get(url + path + '.json').json()
-        message += f'<b>• {magisk_type}</b>:\nInstaller - <a href="{data["magisk"]["link"]}">v{data["magisk"]["version"]}</a> \n' \
-                    f'Manager - <a href="{data["app"]["link"]}">v{data["app"]["version"]}</a> \n' \
-                    f'Uninstaller - <a href="{data["uninstaller"]["link"]}">{magisk_type}</a> \n'
+        message += f'<b>• {magisk_type}</b>:\nMagisk - <a href="{canary + data["magisk"]["link"]}">V{data["magisk"]["version"]}</a> \n' \
+                    f'App - <a href="{canary + data["app"]["link"]}">v{data["app"]["version"]}</a> \n' \
+                    f'Uninstaller - <a href="{canary + data["uninstaller"]["link"]}">{magisk_type}</a> \n'
     await bot.send_message(
                 chat_id=update.chat.id,
                 text=message,
