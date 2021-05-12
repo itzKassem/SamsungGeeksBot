868c8577991739dd130ae68ad9ff080f20b04f1a
D Avinash Kumar <avinashd2904@gmail.com>
2021-05-12 21:48:52 +0530

    Magisk: New Link for json, as OLD got deprecated

--- sgx_bot/plugins/magisk.py
+++ sgx_bot/plugins/magisk.py
@@ -6,23 +6,18 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["magisk", "root"]))
 async def magisk(bot, update):
-    url = "https://raw.githubusercontent.com/topjohnwu/magisk_files/"
+    url = "https://raw.githubusercontent.com/topjohnwu/magisk-files/master/"
     message = "Latest Magisk Releases:\n"
     for magisk_type, path in {
-        "Stable": "master/stable",
-        "Beta": "master/beta",
-        "Canary": "canary/canary",
+        "Stable": "stable",
+        "Beta": "beta",
+        "Canary": "canary",
     }.items():
-        canary = (
-            "https://github.com/topjohnwu/magisk_files/raw/canary/"
-            if magisk_type == "Canary"
-            else ""
-        )
         data = get(url + path + ".json").json()
         message += (
             f"<b>• {magisk_type}</b>:\n"
-            f'- <a href="{canary + data["app"]["link"]}">APP - v{data["app"]["version"]}</a> \n'
-            f'- <a href="{canary + data["uninstaller"]["link"]}">Uninstaller- {magisk_type}</a> \n'
+            f'- <a href="{data["app"]["link"]}">APP - v{data["app"]["version"]}</a> \n'
+            f'- <a href="{data["uninstaller"]["link"]}">Uninstaller- {magisk_type}</a> \n'
         )
     await bot.send_message(
         chat_id=update.chat.id, text=message, disable_web_page_preview=True
